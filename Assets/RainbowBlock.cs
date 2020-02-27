3a1ea4b3b793e1040a450ee4e7561ef30e7960b4
Gourd (cole) <cmedley@uci.edu>
2020-02-26 17:54:24 -0800

    court / block prefabs / mmaterials
    still untested

---
+++ Assets/RainbowBlock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainbowBlock : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //make sure the collission actually happens with a ball then destroy this block
+        if (other.gameObject.tag == "ball")
+        {
+            print("boing!");
+
+            //do soemthing here to reverse the velocity of the pong fall
+            Destroy(this.gameObject);
+        }
+    }
+}
