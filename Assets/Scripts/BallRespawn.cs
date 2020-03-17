aa7f026c9f0e5ab9a09118567ac67ea599f3a775
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-03-16 17:28:15 -0700

    Ball Destroyer.unity built

---
+++ Assets/Scripts/BallRespawn.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallRespawn : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Ball")
+        {
+            other.gameObject.transform.position = this.gameObject.transform.position;
+        }
+    }
+}
