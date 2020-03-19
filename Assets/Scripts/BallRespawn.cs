9d61e0c3ebbaec43ac425993f69a4d2e50285a14
SupremeEmperor <44217972+SupremeEmperor@users.noreply.github.com>
2020-03-19 15:45:02 -0700

    Did some end game stuff

--- Assets/Scripts/BallRespawn.cs
+++ Assets/Scripts/BallRespawn.cs
@@ -1,14 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class BallRespawn : MonoBehaviour
 {
+    [SerializeField] string winner;
+    [SerializeField] GameObject canvas;
+    [SerializeField] GameObject text;
+    float time = 0;
+    [SerializeField] int timeToWait = 10;
+    bool countdown;
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Ball")
         {
-            other.gameObject.transform.position = this.gameObject.transform.position;
+            text.GetComponent<TextMeshProUGUI>().text = "THE WINNER IS:\n" + winner;
+            time = Time.time;
+            countdown = true;
+        }
+    }
+
+    private void Update()
+    {
+        if(countdown == true)
+        {
+            Debug.Log(time);    
+            if(Time.time - time >= 1)
+            {
+                time = Time.time;
+                timeToWait--;
+            }
+            if(timeToWait <= 5)
+            {
+                text.GetComponent<TextMeshProUGUI>().text = "New Game in:\n" + timeToWait;
+            }
+            if(timeToWait <= 0)
+            {
+                SceneManager.LoadScene("ColeScene");
+            }
         }
     }
 }
